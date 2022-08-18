218a743651df480a6116d59b3517c7da5ba765fe
Fatma Tasdemir <f.tasdemir571@gmail.com>
2022-08-18 23:29:03 +0200

    V3

--- src/C01.java
+++ src/C01.java
@@ -4,6 +4,6 @@ public class C01 {
     public static void main(String[] args) {
         System.out.println("1.Deneme");
          System.out.println("1.Deneme Github");
-       
+        System.out.println("1.Deneme master");
     }
 }
